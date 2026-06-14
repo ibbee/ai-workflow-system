8159fd5435528f4b27ec15f8c92fe5c7d6cfaf57
ibbee <89143882+ibbee@users.noreply.github.com>
2026-06-14 07:56:11 +0500

    batch analysis code restructuring

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ app = FastAPI()
 VALID_MODES = list(mode_to_model.keys())
 
 @app.post('/upload-pdf/')
-def upload_pdf(file: UploadFile = File(...),mode:str = 'resume'):
-    if file.content_type != 'application/pdf':
-        raise HTTPException(
-            status_code=400,
-            detail='Only PDFs are allowed'
-        )
+def upload_pdf(files: list[UploadFile] = File(...),mode:str = 'resume'):
+    for pdf in files:
+        if pdf.content_type != 'application/pdf':
+            raise HTTPException(
+                status_code=400,
+                detail=f'Only PDFs are allowed, {pdf.filename} is an invalid file'
+            )
     if mode not in VALID_MODES:
             raise HTTPException(
                  status_code=400,
                  detail="Unsupported analysis mode."
             )
     try:
-        result = workflow.pdf_analysis(mode,file)
-        return {'file-name':file.filename,
-                'analysis':result.model_dump()}
+        api_output = []
+        for pdf in files:
+            result = workflow.pdf_analysis(mode,pdf)
+            api_output.append({'file-names':pdf.filename,
+                    'analysis': result.model_dump()})
+        return {'results' : api_output}
     except Exception as e:
         raise HTTPException(
             status_code=500,
